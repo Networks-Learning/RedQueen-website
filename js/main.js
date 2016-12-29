9aa40859979e9c00443d91e855456ac950d63191
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-12-29 22:32:39 +0100

    Demo the feed visualization.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var feed_vis = function () {
     /* Visualize the feed of the user. */
     var feed_length = 10;
-    var tweet_height = 10, tweet_width = 50;
-    var width = 100;
+    var width = 100, height  = 100;
+    var tweet_height = height / (feed_length + 2), tweet_width = 50;
     var tweet_colors = {
-        'our': 'steelblue',
-        'other': 'lilac'
+        'user': '#1E90FF',
+        'other': '#FF008B'
     };
 
     function tweet_flow(selection) {
@@ -25,16 +25,23 @@ var feed_vis = function () {
                     function (d, i) {
                         // If the source is 'user', enter from left, else from
                         // right.
-                        return d.source === 'user' ? 0 : 100;
+                        return d.source === 'user' ? 0 : 100 - tweet_width / 2;
                     })
                 .attr('y', 0)
-                .attr('width', tweet_width)
-                .attr('height', tweet_height)
-                .attr('fill', function (d) { return tweet_colors['d']; })
+                .attr('width', tweet_width / 2)
+                .attr('height', tweet_height / 2)
+                .attr('fill', function (d) { return tweet_colors[d.source]; })
+                .attr('stroke', 'rgba(0, 0, 0, 0.25)')
+                .attr('stroke-width', 0.5)
+                .attr('opacity', 1e-6)
               .merge(tweets)
                 .transition()
                 .attr('x', (width - tweet_width) / 2) // Centers tweets
-                .attr('y', function (d, i) { return tweet_height * (i + 1); });
+                .attr('y', function (d, i) { return tweet_height * (i + 1); })
+                .attr('width', tweet_width)
+                .attr('height', tweet_height)
+                .attr('transform', 'scale(1)')
+                .attr('opacity', 1.0);
 
             tweets.exit()
                 .transition()
@@ -68,17 +75,18 @@ fetch('data/example1.json')
             .call(tweet_flow);
 
     function update_randomly() {
-        tweets.push({
+        var rand = Math.random();
+        tweets.unshift({
             id: id,
-            source: (Math.random() < 0.25) ? 'user' : 'other'
+            source: (rand < 0.25) ? 'user' : 'other'
         });
         id = id + 1;
-        tweets = tweets.slice(10);
+        tweets = tweets.slice(0, 10);
         d3.select("#redqueen-demo")
                     .data([tweets])
                     .call(tweet_flow);
 
-        setTimeout(update_randomly, 1000);
+        setTimeout(update_randomly, Math.random() * 500);
     }
 
     update_randomly();
